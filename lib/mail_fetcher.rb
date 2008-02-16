9d4f8ca21f8fa26608451001c18443a8a238473a
siannopollo <siannopollo@db1baf47-51e4-401b-90a3-79adba8d9aa3>
2008-02-16 00:24:34 +0000

    Initial import
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mail-fetcher/trunk@2 db1baf47-51e4-401b-90a3-79adba8d9aa3

---
+++ lib/mail_fetcher.rb
@@ -0,0 +1,91 @@
+require File.dirname(__FILE__) + "/net/pop"
+require File.dirname(__FILE__) + "/net/imap"
+require "yaml"
+
+class MailFetcher
+  class << self
+    attr_accessor :mailer_class, :config, :access
+    
+    def mailer_class
+      @mailer_class.to_s.classify.constantize unless @mailer_class.nil?
+    end
+    
+    # Configuration options:
+    # * <tt>mailer_methods</tt> - An array of symbols of the methods you wish the mailer_class to invoke on each
+    #   fetched email. Defaults to :receive.
+    # * <tt>keep</tt> - Tells the fetcher whether to keep the emails. :keep => true keeps the fetcher from
+    #   deleting emails. :keep is implied to be true if :delete_if is used. Fetcher will delete all mail by default.
+    # * <tt>delete_if</tt> - A block to pass in that is run against each email to determine whether it should be
+    #   deleted from the server. If the block evaluates to true, the email is deleted. All other emails are left intact.
+    # * <tt>env</tt> - Tells the fetcher which configuration in mailer.yml to use. Defaults to ENV["RAILS_ENV"].
+    # * <tt>finish</tt> - So, the Net::POP docs say to close the connection with pop#finish, but for some odd
+    #   reason calling the below function when connected to GMail resets POP3 access and won't allow you to reconnect.
+    #   This plugin was tested with a GMail account, hence the need for this option.
+    # * <tt>manipulate_connection</tt> - Used in conjunction with IMAP, in that it passes the IMAP object to the
+    #   mailer_class to do whatever it wishes with the connection (since IMAP allows for more features than POP3).
+    #   Just pass in the name of the method(s) to pass the IMAP object to: :manipulate_connection => :use_imap.
+    def fetch(options={})
+      options = prepare_options(options)
+      check_mailer!(options)
+      self.config ||= YAML.load_file("#{RAILS_ROOT}/config/mail_fetcher.yml")[options[:env] || ENV["RAILS_ENV"]]
+      send :"fetch_#{access || "imap"}", options
+    end
+    
+    def fetch_pop(options={})
+      Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
+      pop = Net::POP3.new(config[:server]).start(config[:username], config[:password])
+      
+      pop.mails.each do |mail|
+        email = TMail::Mail.parse(mail.pop)
+        [options[:mailer_methods]].flatten.each {|method| mailer_class.send(method, email)}
+        mail.delete if (options[:delete_if] && options[:delete_if].call(email))
+        mail.delete unless options[:keep]
+      end
+      pop.finish if options[:finish]
+    end
+    
+    def fetch_imap(options={})
+      imap = Net::IMAP.new(config[:server], config[:port], true)
+      imap.login(config[:username], config[:password])
+      
+      if options[:manipulate_connection]
+        [options[:manipulate_connection]].flatten.each {|method| mailer_class.send(method, imap)}
+      else
+        imap.examine('INBOX')
+        imap.search(['ALL']).each do |message_id|
+          email = TMail::Mail.parse(imap.fetch(message_id,'RFC822')[0].attr['RFC822'])
+          [options[:mailer_methods]].flatten.each {|method| mailer_class.send(method, email)}
+          imap.store(message_id, "+FLAGS", [:Deleted]) if (options[:delete_if] && options[:delete_if].call(email))
+          imap.store(message_id, "+FLAGS", [:Deleted]) unless options[:keep]
+        end
+        imap.close
+      end
+    end
+    
+    protected
+    def check_mailer!(options={})
+      raise NoMailerError if mailer_class.nil?
+      raise MailerClassInterfaceError if ([options[:mailer_methods]].flatten.size == 1 && !mailer_class.respond_to?(:receive))
+    end
+    
+    def prepare_options(options)
+      returning options do |opts|
+        opts[:mailer_methods] ||= :receive
+        opts[:keep] = true if opts[:delete_if]
+        opts[:finish] ||= false
+      end
+    end
+  end
+  
+  class NoMailerError < StandardError
+    def message
+      "A mailer_class must be defined before you can fetch mail"
+    end
+  end
+  
+  class MailerClassInterfaceError < StandardError
+    def message
+      "The mailer_class should at least respond to #receive.\nAlternately, try passing in :mailer_methods => [:method1, :method2] to MailFetcher.fetch"
+    end
+  end
+end
---
+++ spec/models/mail_fetcher_spec.rb
@@ -0,0 +1,190 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe MailFetcher do
+  it "should exist" do
+    MailFetcher.should be_a_kind_of(Class)
+  end
+  
+  it "should raise error if the mailer class isn't set when trying to fetch" do
+    lambda{MailFetcher.fetch}.should raise_error(MailFetcher::NoMailerError)
+  end
+  
+  describe "mailer class" do
+    class MailyMcMailer; end
+    
+    it "should exist" do
+      MailFetcher.should respond_to(:mailer_class)
+    end
+    
+    it "should be set" do
+      MailFetcher.mailer_class = :maily_mc_mailer
+      MailFetcher.mailer_class.should == MailyMcMailer
+    end
+    
+    describe "interface" do
+      before(:each) do
+        MailFetcher.mailer_class = :maily_mc_mailer
+      end
+      
+      it "should respond to receive if no mailer_methods are passed in" do
+        block = lambda {MailFetcher.send(:check_mailer!, :mailer_methods => [:receive])}
+        block.should raise_error(MailFetcher::MailerClassInterfaceError)
+        
+        MailyMcMailer.should_receive(:respond_to?).with(:receive).and_return(true)
+        block = lambda {MailFetcher.send(:check_mailer!, :mailer_methods => [:receive])}
+        block.should_not raise_error(MailFetcher::MailerClassInterfaceError)
+      end
+    end
+  end
+  
+  describe "fetching" do
+    before(:all) do
+      ActionMailer::Base.delivery_method = :smtp
+      ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.smtp_settings = {
+        :address        => "smtp.gmail.com", 
+        :port           => 587, 
+        :domain         => nil, 
+        :user_name      => "mail.fetcher.test@gmail.com", 
+        :password       => "mailfetchertest", 
+        :authentication => :login
+      }
+      
+      MailFetcher.mailer_class = :mailer
+    end
+    
+    
+    describe "POP access" do
+      before(:all) do
+        MailFetcher.config = {
+          :server   => "pop.gmail.com",
+          :username => "mail.fetcher.test@gmail.com",
+          :password => "mailfetchertest"
+        }
+        MailFetcher.access = :pop
+      end
+      
+      after(:each) do
+        Mailer::EMAILS.clear
+      end
+      
+      it "should fetch emails from the specified email" do
+        Mailer.deliver_test_email
+        Mailer::EMAILS.size.should == 0
+        MailFetcher.fetch
+        Mailer::EMAILS.size.should > 0
+      end
+      
+      it "should not delete emails if :keep is true" do
+        Mailer.deliver_test_email
+        Mailer::EMAILS.size.should == 0
+        MailFetcher.fetch(:keep => true)
+        size = Mailer::EMAILS.size
+        size.should > 0
+        
+        MailFetcher.fetch
+        Mailer::EMAILS.size.should > size
+      end
+      
+      it "should allow a block to be passed to determine if an email should be deleted" do
+        Mailer::EMAILS.size.should == 0
+        subject = "G'day mate! - #{Time.now.strftime("%I:%M %p")}"
+        Mailer.deliver_test_email(:subject => subject)
+        
+        delete_if = lambda {|email| email.subject =~ /#{Regexp.escape(subject)}/}
+        MailFetcher.fetch(:delete_if => delete_if)
+        
+        Mailer::EMAILS.clear
+        MailFetcher.fetch
+        Mailer::EMAILS.select {|e| e.subject =~ /#{Regexp.escape(subject)}/}.size.should == 0
+      end
+    end
+    
+    describe "IMAP access" do
+      before(:all) do
+        MailFetcher.config = {
+          :server   => "imap.gmail.com",
+          :port     => 993,
+          :username => "mail.fetcher.test@gmail.com",
+          :password => "mailfetchertest"
+        }
+        MailFetcher.access = :imap
+      end
+      
+      before(:each) do
+        Mailer::EMAILS.clear
+        Mailer::IMAP_EMAILS.clear
+      end
+      
+      it "should fetch emails from the specified address" do
+        Mailer.deliver_test_email
+        Mailer::EMAILS.size.should == 0
+        MailFetcher.fetch
+        Mailer::EMAILS.size.should > 0
+      end
+      
+      it "should not delete emails if :keep is true" do
+        Mailer.deliver_test_email
+        Mailer::EMAILS.size.should == 0
+        MailFetcher.fetch(:keep => true)
+        size = Mailer::EMAILS.size
+        size.should > 0
+        
+        MailFetcher.fetch
+        Mailer::EMAILS.size.should > size
+      end
+      
+      it "should allow a block to be passed to determine if an email should be deleted" do
+        Mailer::EMAILS.size.should == 0
+        subject = "G'day mate! - #{Time.now.strftime("%I:%M %p")}"
+        Mailer.deliver_test_email(:subject => subject)
+        
+        delete_if = lambda {|email| email.subject =~ /#{Regexp.escape(subject)}/}
+        MailFetcher.fetch(:delete_if => delete_if)
+        
+        Mailer::EMAILS.clear
+        MailFetcher.fetch
+        Mailer::EMAILS.select {|e| e.subject =~ /#{Regexp.escape(subject)}/}.size.should == 0
+      end
+      
+      it "should allow the imap object to be passed to the mailer_class to do whatever it wishes with the connection" do
+        Mailer.deliver_test_email
+        Mailer::IMAP_EMAILS.size.should == 0
+        Mailer::EMAILS.size.should == 0
+        
+        MailFetcher.fetch(:manipulate_connection => :use_imap)
+        Mailer::IMAP_EMAILS.size.should > 0
+        Mailer::EMAILS.size.should == 0
+      end
+    end
+  end
+end
+
+class Mailer < ActionMailer::Base
+  EMAILS = []
+  IMAP_EMAILS = []
+  
+  def test_email(options={})
+    @recipients, @from, @subject = 
+    (options[:recipients] || "mail.fetcher.test@gmail.com"),
+    (options[:from] || "the_test@example.com"),
+    (options[:subject] || "Just Testing")
+  end
+  
+  def render_message(method_name, body)
+    "Let me know if this works... please?"
+  end
+  
+  def self.receive(email)
+    Mailer::EMAILS << email
+  end
+  
+  def self.use_imap(imap)
+    imap.examine('INBOX')
+    imap.search(['ALL']).each do |message_id|
+      IMAP_EMAILS << TMail::Mail.parse(imap.fetch(message_id,'RFC822')[0].attr['RFC822'])
+      imap.store(message_id, "+FLAGS", [:Deleted])
+    end
+    imap.close
+  end
+end
